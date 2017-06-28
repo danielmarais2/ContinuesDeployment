7564d7fca4b303d99046c3570e8b19b9b4f85894
Daniel.Marais <Daniel.Marais@SAGCPTNBDANIELM>
2017-06-28 17:51:36 +0200

    Add project files.

---
+++ ContinuesDeployment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContinuesDeployment.Startup))]
+namespace ContinuesDeployment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
